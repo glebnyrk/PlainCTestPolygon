cba7d8220fe148f0d5cbd656787d51737f3935a2
Nyrkov Gleb <zenyt4030@gmail.com>
2025-04-02 14:05:48 +0300

    some tests with kyber-1024

--- test1.c
+++ test1.c
@@ -8,21 +8,21 @@ int main(){
         exit(-1);
     }
     printf("sodium init     :)\n");
-    // Инициализация Kyber-768
-    OQS_KEM *kem = OQS_KEM_new(OQS_KEM_alg_kyber_768);
+    // Инициализация Kyber-1024
+    OQS_KEM *kem = OQS_KEM_new(OQS_KEM_alg_kyber_1024);
     if (kem == NULL) {
-        printf("Ошибка: Kyber-768 не поддерживается\n");
+        printf("Ошибка: Kyber-1024 не поддерживается\n");
         return 1;
     }
     //Память для публичного ключа
-    uint8_t public_key[OQS_KEM_kyber_768_length_public_key];
+    uint8_t public_key[OQS_KEM_kyber_1024_length_public_key];
     //Память для приватного ключа
-    uint8_t secret_key[OQS_KEM_kyber_768_length_secret_key];
+    uint8_t secret_key[OQS_KEM_kyber_1024_length_secret_key];
     //Память для результата шифрования
-    uint8_t ciphertext[OQS_KEM_kyber_768_length_ciphertext];
+    uint8_t ciphertext[OQS_KEM_kyber_1024_length_ciphertext];
     //хз для чего это
-    uint8_t shared_secret_enc[OQS_KEM_kyber_768_length_shared_secret];
-    uint8_t shared_secret_dec[OQS_KEM_kyber_768_length_shared_secret];
+    uint8_t shared_secret_enc[OQS_KEM_kyber_1024_length_shared_secret];
+    uint8_t shared_secret_dec[OQS_KEM_kyber_1024_length_shared_secret];
 
     // Генерация ключей
     OQS_KEM_keypair(kem, public_key, secret_key);
@@ -37,29 +37,14 @@ int main(){
     printf("Деинкапсуляция прошла\n");
 
     // Проверка совпадения секретов
-    if (memcmp(shared_secret_enc, shared_secret_dec, OQS_KEM_kyber_768_length_shared_secret) == 0) {
+    if (memcmp(shared_secret_enc, shared_secret_dec, OQS_KEM_kyber_1024_length_shared_secret) == 0) {
         printf("Общие секреты совпадают\n");
     } else {
         printf("Ошибка: секреты не совпадают\n");
         return 1;
     }
-
-    // Шифрование сообщения с помощью libsodium
-    unsigned char message[] = "Тест EpsilonSecure";
-    unsigned char nonce[crypto_secretbox_NONCEBYTES];
-    unsigned char encrypted[crypto_secretbox_MACBYTES + sizeof(message)];
-    randombytes_buf(nonce, sizeof(nonce));
-
-    crypto_secretbox_easy(encrypted, message, sizeof(message), nonce, shared_secret_enc);
-    printf("Сообщение зашифровано\n");
-
-    // Расшифровка
-    unsigned char decrypted[sizeof(message)];
-    if (crypto_secretbox_open_easy(decrypted, encrypted, sizeof(encrypted), nonce, shared_secret_dec) == 0) {
-        printf("Сообщение расшифровано: %s\n", decrypted);
-    } else {
-        printf("Ошибка расшифровки\n");
+    for(int i = 0; i < OQS_KEM_kyber_1024_length_shared_secret; i++){
+        printf("%02x",shared_secret_enc[i]);
     }
-
-    OQS_KEM_free(kem);
+    printf("\n%i\n",OQS_KEM_kyber_1024_length_shared_secret);
 }
